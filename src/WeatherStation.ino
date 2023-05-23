56aeb5e68366eff77223d350b9d4d7eb6150041f
Lucy Smith <lucy.rose.smith@icloud.com>
2023-05-23 19:23:46 +0100

    first commit

---
+++ src/WeatherStation.ino
@@ -0,0 +1,18 @@
+/*
+ * Project WeatherStation
+ * Description: A level project
+ * Author: Lucy Smith 
+ * Date: 23/05/23
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
